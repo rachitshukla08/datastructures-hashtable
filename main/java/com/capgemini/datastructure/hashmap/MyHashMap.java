782bd7ef1cb2bd15b882048b4704d6fa82c1f36a
unknown <rachitshukla08@gmail.com>
2020-10-10 17:18:03 +0530

    Added: MyHashMap, MyMapNode, MyHashMapTest classes to create and test hash map for UC1

---
+++ main/java/com/capgemini/datastructure/hashmap/MyHashMap.java
@@ -0,0 +1,37 @@
+package com.capgemini.datastructure.hashmap;
+
+import com.capgemini.datastructure.linkedlist.MyLinkedList;
+
+/**
+ * @author Rachit
+ */
+public class MyHashMap<K extends Comparable<K>, V extends Comparable<V>> {
+	MyLinkedList<K> myLinkedList;
+	
+	/**
+	 * Constructor
+	 */
+	public MyHashMap() {
+		this.myLinkedList = new MyLinkedList<>();
+	}
+
+	public V get(K key) {
+		MyMapNode<K,V> myMapNode = (MyMapNode<K,V>) this.myLinkedList.search(key);
+		return (myMapNode==null)?null:myMapNode.getValue();
+	}
+
+	public void add(K key, V value) {
+		MyMapNode<K,V> myMapNode = (MyMapNode<K,V>) this.myLinkedList.search(key);
+		if(myMapNode == null) {
+			myMapNode = new MyMapNode<>(key, value);
+			this.myLinkedList.append(myMapNode);
+		}
+		else 
+			myMapNode.setValue(value);
+	}
+	
+	@Override
+	public String toString() {
+		return "MyHashMapNodes{"+ myLinkedList+'}';
+	}
+}
---
+++ main/java/com/capgemini/datastructure/linkedlist/INode.java
@@ -0,0 +1,10 @@
+package com.capgemini.datastructure.linkedlist;
+
+public interface INode<K extends Comparable<K>> {
+	K getKey();
+	void setKey(K key);
+	
+	INode getNext();
+	void setNext(INode next);
+}
+
