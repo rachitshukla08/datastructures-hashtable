ed0d9b45130b463e7f5b8e29fd8ca183d58f963a
unknown <rachitshukla08@gmail.com>
2020-10-12 14:28:09 +0530

    Modified: MyBinaryTreeTest class to add test case for adding given numbers

---
+++ test/java/com/capgemini/datastructure/binarytree/MyBinaryTreeTest.java
@@ -0,0 +1,38 @@
+package com.capgemini.datastructure.binarytree;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MyBinaryTreeTest {
+	
+	@Test
+	public void given3Numbers_WhenAddedToBinaryTree_ShouldReturnSizeThree() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		int size = myBinaryTree.getSize();
+		assertEquals(3, size);
+	}
+	
+	@Test
+	public void givenNumbersAsInput_WhenAddedToBinaryTree_ShouldReturnSize13() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		int size = myBinaryTree.getSize();
+		assertEquals(13, size);
+	}
+}
